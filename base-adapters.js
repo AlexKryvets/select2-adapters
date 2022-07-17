8f6f3f656628c1e51e5c59be77e5f37da3e69e40
alex.kryvets <alex.kryvets@gmail.com>
2022-07-17 22:58:41 +0300

    release 1.2.1

--- base-adapters.js
+++ base-adapters.js
@@ -1,3 +1,4 @@
+import * as jQuery from 'jquery';
 const SelectAdapter = jQuery.fn.select2.amd.require('select2/data/select');
 const ArrayAdapter = jQuery.fn.select2.amd.require('select2/data/array');
 export {SelectAdapter, ArrayAdapter};
